eb874ac053f301dc3749368e5b966392dc3b3ca2
github username <github email address>
2019-05-27 12:39:26 -0700

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Comment.destroy_all
+User.destroy_all
+Post.destroy_all
+
+User.create(username: "macdog", email: "macdog1233@gmail.com")
+User.create(username: "tranglethorn", email: "trangle@gmail.com")
+
+Post.create(title: "To Be", content: "A World Journey")
+Post.create(title: "Listing Things", content: "Cats, dogs, blubber, milk, superpowers")
+
+Comment.create(content: "What is this?", user_id: 1, post_id: 2)
+Comment.create(content: "Simply glory", user_id: 1, post_id: 1)
+Comment.create(content: "Seen Better", user_id: 2, post_id: 1)
